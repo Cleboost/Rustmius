967993ab53dbe9d5965cd8ea5c9f610474a4bfe2
Cleboost <clement.balarot@gmail.com>
2026-05-11 22:22:17 +0200

    chore: remove scratch.rs temporary file

--- scratch.rs
+++
@@ -1,5 +0,0 @@
-use gtk4::prelude::*;
-fn main() {
-    let formats = gtk4::gdk::ContentFormats::builder().add_type(gtk4::gdk::FileList::static_type()).build();
-    let _target = gtk4::DropTarget::builder().formats(&formats).build();
-}
